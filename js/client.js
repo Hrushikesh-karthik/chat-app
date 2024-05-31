f32ff0c9dddfcaae914b077d0e7d6e2f8d51ff03
U Hrushikesh Karthik <143626493+Hrushikesh-karthik@users.noreply.github.com>
2024-05-31 17:15:44 +0530

    Add files via upload

---
+++ js/client.js
@@ -0,0 +1,39 @@
+const socket = io('http://localhost:8000');
+const form = document.getElementById('send-container');
+const messageInput = document.getElementById('messageInp');
+const messageContainer = document.querySelector(".container");
+var audio=new Audio('tune.mp3');
+if(position=='left'){
+    audio.play();
+}
+
+const append = (message, position) => {
+    const messageElement = document.createElement('div');
+    messageElement.innerText = message;
+    messageElement.classList.add('message');
+    messageElement.classList.add(position);
+    messageContainer.append(messageElement);
+}
+
+// Prompt for user's name and emit 'new-user-joined' event
+const name = prompt("Enter your name to join");
+socket.emit('new-user-joined', name);
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    const message = messageInput.value;
+    append(`You: ${message}`, 'right');
+    socket.emit('send', message);
+    messageInput.value = '';
+});
+
+socket.on('user-joined', name => {
+    append(`${name} joined the chat`, 'right');
+});
+
+socket.on('receive', data => {
+    append(`${data.name}: ${data.message}`, 'left');
+});
+socket.on('left', data => {
+    append(`${data.name}: left the chat`, 'right');
+});
